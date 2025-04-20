24ab7a2031a494de87763a95aa3b64d172811f87
IjazkhanAfridi <iafridi640@gmail.com>
2025-04-20 05:10:06 +0500

    resovle uniqueness issue

--- models/Discente.js
+++ models/Discente.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const discenteSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   cognome: { type: String, required: true },
-  codiceFiscale: { type: String, required: true,unique:true },
+  codiceFiscale: { type: String, required: true},
   indirizzo: { type: String, required: true },
   città: { type: String, required: true },
   regione: { type: String, required: true },
-  email: { type: String, required: true,unique:true },
+  email: { type: String, required: true },
   telefono: { type: String, required: true },
   patentNumber: { type: [String] },
   dateOfBirth: { type: Date, required: true },
@@ -22,5 +22,9 @@ const discenteSchema = new mongoose.Schema({
 }, {
   timestamps: true,
 });
+
+discenteSchema.index({ codiceFiscale: 1, userId: 1 }, { unique: true });
+discenteSchema.index({ email: 1, userId: 1 }, { unique: true });
+
 
 module.exports = mongoose.model('Discente', discenteSchema);
